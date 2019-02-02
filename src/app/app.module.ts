517bba69ff59d2e008e79ad60022393340b473b7
shaileshtanwar <shaileshtanwar@gmail.com>
2019-02-02 12:30:40 +0530

    updated tile component.
    added html /css code. created service to fetch data and added dummy data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http'; 
+
 import { AppComponent } from 'src/app/app.component';
 import { TileComponent } from 'src/app/tile/tile.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
@@ -8,6 +10,7 @@ import { ToolbarModule } from 'src/app/toolbar/toolbar.module';
 import { CoreModule } from 'src/app/core/core.module';
 import { MaterialModule } from 'src/app/material.module';
 import { HomeComponent } from 'src/app/home/home.component';
+import { TileService } from 'src/app/tile/tile.service';
 
 
 @NgModule({
@@ -18,13 +21,14 @@ import { HomeComponent } from 'src/app/home/home.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ToolbarModule,
     CoreModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [TileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
