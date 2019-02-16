164811681421403f380fc4db0b70c1c863eb3b06
shaileshtanwar <shaileshtanwar@gmail.com>
2019-02-16 17:05:00 +0530

    updated login button

--- src/app/toolbar/toolbar.module.ts
+++ src/app/toolbar/toolbar.module.ts
@@ -4,13 +4,15 @@ import { ToolbarComponent } from './toolbar.component';
 import { MatToolbarModule } from '@angular/material';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from 'src/app/material.module';
 @NgModule({
   declarations: [ToolbarComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatSlideToggleModule,
-    RouterModule
+    RouterModule,
+    MaterialModule
   ],
   exports: [ToolbarComponent]
 })
