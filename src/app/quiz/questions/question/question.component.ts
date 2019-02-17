d67b0dc9e06b526cbc4eff5a07ca1215e497be39
shaileshtanwar <shaileshtanwar@gmail.com>
2019-02-17 18:17:16 +0530

    added formarray for options of a question in quiz

--- src/app/quiz/questions/question/question.component.ts
+++ src/app/quiz/questions/question/question.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionService } from 'src/app/quiz/questions/question/question.service';
 import { Input } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-question',
@@ -10,23 +10,32 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class QuestionComponent implements OnInit {
 
-  constructor(private questionService: QuestionService) { }
+  constructor(private questionService: QuestionService, private formBuilder: FormBuilder) { }
   public questions: QuestionVM[];
   public questionIndex: number = 0;
   public totalQuestion: number = 3;
-  public options: FormGroup;
+  public optionFormGroup: FormGroup;
   @Input() topic: string;
   ngOnInit() {
     this.questionService.getQuestionData(this.topic).subscribe((questionsData: QuestionVM[]) => {
       this.questions = questionsData;
       console.log(this.questions);
     });
-    this.options = new FormGroup({
-      op1: new FormControl(),
-      op2: new FormControl(),
-      op3: new FormControl(),
-      op4: new FormControl()
-    })
+    this.optionFormGroup = this.formBuilder.group({
+      optionsArray: this.formBuilder.array([])
+    });
+    for (var i = 0; i < this.totalQuestion; i++) {
+      (<FormArray>this.optionFormGroup.get('optionsArray')).push(this.createOptions());
+    }
+  }
+
+  createOptions(): FormGroup {
+    return this.formBuilder.group({
+      op1: false,
+      op2: false,
+      op3: false,
+      op4: false
+    });
   }
 
   public nextQuestion() {
