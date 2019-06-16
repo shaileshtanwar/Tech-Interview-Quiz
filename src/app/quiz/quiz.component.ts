faba86589c3641b6a962d0d269e44cfd80717be2
shaileshtanwar <shaileshtanwar@gmail.com>
2019-06-16 21:45:36 +0530

    updated quiz component

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -8,26 +8,10 @@ import { TileService } from 'src/app/tile/tile.service';
   styleUrls: ['./quiz.component.scss']
 })
 export class QuizComponent implements OnInit {
-  // isLinear = true;
-  // firstFormGroup: FormGroup;
-  // secondFormGroup: FormGroup;
-  // levels: Levels[] = [
-  //   { value: '1', viewValue: 'Easy' },
-  //   { value: '2', viewValue: 'Beginner' },
-  //   { value: '3', viewValue: 'Intermediate' },
-  //   { value: 'w', viewValue: 'Expert' }
-  // ];
   public quizTopic: string = undefined;
   constructor(private _formBuilder: FormBuilder, private tileservice: TileService) { }
 
   ngOnInit() {
-    // this.firstFormGroup = this._formBuilder.group({
-    //   firstNameCtrl: ['', Validators.required],
-    //   lastNameCtrl: ['', Validators.required]
-    // });
-    // this.secondFormGroup = this._formBuilder.group({
-    //   difficultyLevel: ['', Validators.required]
-    // });
     this.quizTopic = this.tileservice.quiztopic;
   }
 
